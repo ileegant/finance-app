82d31a2b027e42e0f80014f30237a9ee5c69dfae
ileegant <osukhoruchko@gmail.com>
2026-03-11 12:02:38 +0200

    dfadfd

--- src/components/transactions/TransactionsHeader.tsx
+++ src/components/transactions/TransactionsHeader.tsx
@@ -1,26 +1,16 @@
-import { ArrowBigLeft } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function TransactionsHeader() {
   return (
     <div className="flex flex-col gap-4 p-4 rounded-2xl bg-white/80 border border-white/40 shadow-sm">
       <div className="flex justify-between">
-        <div className="flex gap-2">
-          <ArrowBigLeft />
-          <h5>All transactions</h5>
-        </div>
-        <button className="border border-slate-800 hover:text-slate-50 hover:bg-slate-600 text-xs px-2 rounded-xl cursor-pointer">
-          Export Excel
-        </button>
-      </div>
-      <div className="flex gap-2">
-        <button className="text-sm px-4 border bg-slate-600 text-slate-50 border-slate-600 rounded-xl cursor-pointer">
-          All
-        </button>
-        <button className="text-sm px-4 border border-slate-600 rounded-xl cursor-pointer">
-          Income
-        </button>
-        <button className="text-sm px-4 border border-slate-600 rounded-xl cursor-pointer">
-          Expenses
+        <Link to="/">
+          <ChevronLeft />
+        </Link>
+        <h5>All transactions</h5>
+        <button className="text-slate-400 border border-slate-400 text-xs px-2 rounded-xl">
+          Export
         </button>
       </div>
     </div>
--- src/pages/TransactionsPage.tsx
+++ src/pages/TransactionsPage.tsx
@@ -4,8 +4,18 @@ import TransactionsHeader from "../components/transactions/TransactionsHeader";
 export default function TransactionsPage() {
   return (
     <div className="flex flex-col gap-4 justify-center w-120">
-
       <TransactionsHeader />
+      <div className="flex gap-2">
+        <button className="text-sm px-4 border bg-slate-600 text-slate-50 border-slate-600 rounded-xl cursor-pointer">
+          All
+        </button>
+        <button className="text-sm px-4 border border-slate-600 rounded-xl cursor-pointer">
+          Income
+        </button>
+        <button className="text-sm px-4 border border-slate-600 rounded-xl cursor-pointer">
+          Expenses
+        </button>
+      </div>
       <TransactionList />
     </div>
   );
