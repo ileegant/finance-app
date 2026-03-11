77555d00b1d8815bea082d314f6dcbe5f2793787
ileegant <osukhoruchko@gmail.com>
2026-03-11 17:16:15 +0200

    style: change all transactions header position

--- src/components/transactions/TransactionsHeader.tsx
+++ src/components/transactions/TransactionsHeader.tsx
@@ -5,10 +5,12 @@ export default function TransactionsHeader() {
   return (
     <div className="flex flex-col gap-4 p-4 rounded-2xl bg-white/80 border border-white/40 shadow-sm">
       <div className="flex justify-between">
-        <Link to="/">
-          <ChevronLeft />
-        </Link>
-        <h5>All transactions</h5>
+        <div className="flex gap-2">
+          <Link to="/">
+            <ChevronLeft />
+          </Link>
+          <h5>All transactions</h5>
+        </div>
         <button className="text-slate-400 border border-slate-400 text-xs px-2 rounded-xl">
           Export
         </button>
