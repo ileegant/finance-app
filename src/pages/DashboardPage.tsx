2c8c3696e310253664605249b5a0aa262d2046ca
ileegant <osukhoruchko@gmail.com>
2026-03-16 11:32:08 +0200

    fix: remove unused props

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -6,11 +6,11 @@ import { MOCK_TRANSACTIONS } from "../data/mockTransactions";
 export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-4 pt-10 w-120">
-      <TotalBalance change="+5,4" />
+      <TotalBalance />
 
       <div className="flex gap-4">
-        <StatCard change="+12" />
-        <StatCard variant="expenses" change="-14" />
+        <StatCard />
+        <StatCard variant="expenses" />
       </div>
 
       <TransactionList limit={5} showTitle transactions={MOCK_TRANSACTIONS} />
